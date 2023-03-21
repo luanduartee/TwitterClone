728cb659d4460c8aa009127c10a82c6454352a78
Luan Mateus <luanmateus.duarte@gmail.com>
2023-03-21 20:19:19 -0300

    criptografando alguns dados

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,11 +1,13 @@
 <?php
 
+    session_start();
+
     require_once('db.class.php');
 
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
+    $sql = "SELECT usuario, email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
     $objDb = new db();
     $link = $objDb->conecta_mysql();
@@ -18,7 +20,10 @@
 
         if(isset($dados_usuario['usuario'])){
 
-            echo 'usuário existe';
+            $_SESSION['usuario'] = $dados_usuario['usuario'];
+            $_SESSION['email'] = $dados_usuario['email'];
+
+            header('Location: home.php');
 
         }else{
 
