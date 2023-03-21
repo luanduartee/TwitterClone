6f400ae90924d0887e8ba615d7d4150f20fd68fd
Luan Mateus <luanmateus.duarte@gmail.com>
2023-03-21 20:20:55 -0300

    continuação do projeto

--- registra_usuario.php
+++ registra_usuario.php
@@ -4,10 +4,52 @@
 
     $usuario = $_POST['usuario'];
     $email = $_POST['email'];
-    $senha = $_POST['senha'];
+    $senha = md5($_POST['senha']);
 
     $objDb = new db();
     $link = $objDb->conecta_mysql();
+
+    //verificar se o usuário já existe
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' ";
+    if($resultado_id = mysqli_query($link, $sql)){
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['usuario'])){
+
+            echo 'Usuário já cadastrado';
+
+        }else{
+
+            echo 'Usuário não cadastrado, ok, pode cadastrar';
+
+        }
+
+    }else{
+        echo 'Erro ao tentar localizar o registro de usuário';
+    }
+
+    //verificar se o e-mail já existe
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' ";
+    if($resultado_id = mysqli_query($link, $sql)){
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['email'])){
+
+            echo 'Email já cadastrado';
+
+        }else{
+
+            echo 'Email não cadastrado, ok, pode cadastrar';
+
+        }
+
+    }else{
+        echo 'Erro ao tentar localizar o registro de email';
+    }
+
+    die();
 
     $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
 
